3501cd29c47b70a6df78af4a75bcf061e4e574db
sasikumar9211 <sasikumar.balasubramanian92@gmail.com>
2020-08-09 00:09:27 +0530

    mongooseConfigUpdt

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager',{ 
-                    useNewUrlParser :true,
-                    useCreateIndex :true});
+// mongoose.connect('mongodb://127.0.0.1:27017/task-manager',{ 
+//                     useNewUrlParser :true,
+//                     useCreateIndex :true});
 
 // const user = mongoose.model('User',{
 //     name: {
